4d89e9674c6ceed2d7d92c58c94027b6decdc836
RAMCHARAN HANUMANTHU <106550364+HRC-123@users.noreply.github.com>
2024-02-11 16:14:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const app = express();
 
 
 const __dirname = path.resolve();
-app.use(cors());
+app.use(cors({
+    origin:[""],
+  methods:["POST","GET"],
+  credentials:true
+}));
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/',Router);
