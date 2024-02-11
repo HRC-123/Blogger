60df44f0d4dcd9fba23e1a2fc84e0de03297eb79
RAMCHARAN HANUMANTHU <106550364+HRC-123@users.noreply.github.com>
2024-02-11 16:40:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,6 +18,13 @@ app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/',Router);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://blogger-website-eight.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 
 // app.use(express.static(path.join("__dirname","./client/build")));
