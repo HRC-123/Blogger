6d6d99484aef3f876fd7cd23d021b432d8eab62d
RAMCHARAN HANUMANTHU <106550364+HRC-123@users.noreply.github.com>
2024-03-01 19:08:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,21 +9,21 @@ const app = express();
 
 
 const __dirname = path.resolve();
-app.use(cors({
-    origin:["https://blogger-website-eight.vercel.app"],
-  methods:["POST","GET"],
-  credentials:true
-}));
+// app.use(cors({
+//     origin:["https://blogger-website-eight.vercel.app"],
+//   methods:["POST","GET"],
+//   credentials:true
+// }));
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/',Router);
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://blogger-website-eight.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://blogger-website-eight.vercel.app');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 
 
